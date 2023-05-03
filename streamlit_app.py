671ce5d2391620cf58a8397d775595fc2b8a3f02
Joshua Carroll <joshua.carroll@snowflake.com>
2023-05-03 13:25:02 -0700

    Use latest branch, other improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,32 @@
 import streamlit as st
-from st_files_connection import FilesConnection
-import pandas as pd
+
+st.set_page_config(
+    page_title='HF Dataset Explorer',
+    page_icon='🤗'
+)
 
 "# HuggingFace Dataset Explorer"
 
 """
 This app is a quick prototype of using st.experimental_connection
 with HfFileSystem to access HF DataSet data from Streamlit.
-
-It's not doing anything super interesting since all of this can
-be found easily on the HF Hub, but demonstrates how easy it is to set up.
-
-There's probably some cool stuff to do with
-[data editor](https://data-editor.streamlit.app/) here too!
 """
 
+with st.expander("Discussion / caveats"):
+    """
+    It's not doing anything super interesting since all of this can
+    be found easily on the HF Hub, but demonstrates how easy it is to set up.
+
+    There's probably some cool stuff to do with
+    [data editor](https://data-editor.streamlit.app/) here too!
+
+    Works with: datasets where data is in a `data/` folder and stored in csv, jsonl, or parquet files
+    """
+
 with st.echo("below"):
+    import streamlit as st
+    from st_files_connection import FilesConnection
+
     conn = st.experimental_connection('hf', type=FilesConnection)
 
     # Enter a dataset and retrieve a list of data files
@@ -29,14 +40,17 @@ with st.echo("below"):
     datafile_selection = st.selectbox("Pick a data file", file_options)
     nrows = st.slider("Rows to retrieve", value=50)
 
-    # View the first few rows
-    # TODO: add native json support to FilesConnection.read()
-    # so this can be simplified to a one liner
-    @st.cache_data(ttl=3600)
-    def retrieve_data(selection, nrows):
-        with conn.open(selection) as f:
-            return pd.read_json(f, lines=True, nrows=nrows)
-        
     "## Dataset Preview"
-    df = retrieve_data(datafile_selection, nrows)
-    st.dataframe(df, use_container_width=True)
+    kwargs = dict(nrows=nrows, ttl=3600)
+
+    # parquet doesn't neatly support nrows
+    # could be fixed with something like this:
+    # https://stackoverflow.com/a/69888274/20530083
+    import pathlib
+    if pathlib.Path(datafile_selection).suffix == '.parquet':
+        del(kwargs['nrows'])
+    
+    df = conn.read(datafile_selection, **kwargs)
+    st.dataframe(df.head(nrows), use_container_width=True)
+
+    "## Full code for this app"
