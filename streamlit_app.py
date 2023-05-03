94fa260f70d7b9f22b4e61041457347e49df8cd0
Joshua Carroll <joshua.carroll@snowflake.com>
2023-05-03 15:18:07 -0700

    More cleanups and improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,72 @@
 import streamlit as st
+from st_files_connection import FilesConnection
+from pathlib import Path
+from utils import get_files
 
 st.set_page_config(
-    page_title='HF Dataset Explorer',
+    page_title='HuggingFace Dataset Explorer',
     page_icon='🤗'
 )
 
-"# HuggingFace Dataset Explorer"
+"# 🤗 HuggingFace Dataset Explorer"
 
 """
 This app is a quick prototype of using st.experimental_connection
 with HfFileSystem to access HF DataSet data from Streamlit.
+
+View the full app code [here](https://github.com/sfc-gh-jcarroll/hf-connection-demo).
+There's probably some cool stuff to do with
+[data editor](https://data-editor.streamlit.app/) here too!
 """
 
-with st.expander("Discussion / caveats"):
-    """
-    It's not doing anything super interesting since all of this can
-    be found easily on the HF Hub, but demonstrates how easy it is to set up.
+conn = st.experimental_connection('hf', type=FilesConnection)
 
-    There's probably some cool stuff to do with
-    [data editor](https://data-editor.streamlit.app/) here too!
+with st.expander('Find dataset examples'):
+    if 'dataset' not in st.session_state:
+        st.session_state.dataset = "EleutherAI/lambada_openai"
 
-    Works with: datasets where data is in a `data/` folder and stored in csv, jsonl, or parquet files
-    """
-
-with st.echo("below"):
-    import streamlit as st
-    from st_files_connection import FilesConnection
-
-    conn = st.experimental_connection('hf', type=FilesConnection)
-
-    # Enter a dataset and retrieve a list of data files
-    dataset_name = st.text_input(
-        "Enter your dataset of interest",
-        value="EleutherAI/lambada_openai"
-        )
-    dataset = f'datasets/{dataset_name}'
-    file_options = [f['name'] for f in conn.fs.ls(f"{dataset}/data")]
-
-    datafile_selection = st.selectbox("Pick a data file", file_options)
-    nrows = st.slider("Rows to retrieve", value=50)
-
-    "## Dataset Preview"
-    kwargs = dict(nrows=nrows, ttl=3600)
-
-    # parquet doesn't neatly support nrows
-    # could be fixed with something like this:
-    # https://stackoverflow.com/a/69888274/20530083
-    import pathlib
-    if pathlib.Path(datafile_selection).suffix == '.parquet':
-        del(kwargs['nrows'])
+    def set_dataset():
+        st.session_state.dataset = st.session_state._dataset
     
-    df = conn.read(datafile_selection, **kwargs)
-    st.dataframe(df.head(nrows), use_container_width=True)
+    dataset_examples = [
+        "EleutherAI/lambada_openai",
+        "argilla/news-summary",
+        "stanfordnlp/SHP",
+        "HuggingFaceM4/tmp-pmd-synthetic-testing",
+        "google/MusicCaps"
+    ]
+    st.selectbox("Examples", dataset_examples, key="_dataset", on_change=set_dataset)
 
-    "## Full code for this app"
+    """
+    You can also search for datasets on [HuggingFace Hub](https://huggingface.co/datasets).
+    This repo supports datasets with data stored in json, jsonl, csv or parquet format.
+    """
+
+# Enter a dataset and retrieve a list of data files
+dataset_name = st.text_input("Enter your dataset of interest", key='dataset')
+dataset = Path('datasets', dataset_name)
+file_names = get_files(conn, dataset)
+if not file_names:
+    st.warning("No compatible data files found. This app only supports datasets stored in csv, json[l] or parquet format.")
+    st.stop()
+
+# Select a data file and row count to retrieve
+file_selection = st.selectbox("Pick a data file", file_names)
+datafile = Path(dataset, file_selection)
+nrows = st.slider("Rows to retrieve", value=50)
+
+"## Dataset Preview"
+kwargs = dict(nrows=nrows, ttl=3600)
+
+# parquet doesn't neatly support nrows
+# could be fixed with something like this:
+# https://stackoverflow.com/a/69888274/20530083
+if datafile.suffix == '.parquet':
+    del(kwargs['nrows'])
+
+# More often than not people get the extension wrong
+if datafile.suffix == '.json':
+    kwargs['lines'] = True
+
+df = conn.read(datafile, **kwargs)
+st.dataframe(df.head(nrows), use_container_width=True)
